aeed2752bd12a121b3284c59d96f3a03101ae267
mahdi-cli-at-585466297447 <mahdi.azarboon@eficode.com>
2020-03-24 11:50:11 +0200

    removes static amazon-cognito-auth-js and downloads it using npm & yarn

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-const AmazonCognitoIdentity = require("./amazon-cognito-auth.js");
+const AmazonCognitoIdentity = require("amazon-cognito-auth-js");
 
 const apiURL = process.env.ApiRoot;
 
